21eed0f82b8c4db25edcbd14f6a49e07b476e3f6
k.kuzmin <k.kuzmin@npo-echelon.ru>
2016-10-17 16:19:23 +0300

    readygit add README.md  password_strength.py !

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,24 @@
+import re
+import argparse
 
 
 def get_password_strength(password):
-    pass
+    if len(password) < 8:
+        return 1
+    result = round((len(password) % 21)/7 + 1, 1)
+    if re.search('[A-Z]', password):
+        result += 2
+    if re.search('[0-9]', password):
+        result += 2
+    if re.search('[\W]', password):
+        result += 2
+    return result
 
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-p', '--password', help="Input your password", type=str, required=True)
+
+    args = parser.parse_args()
+    print ('Strength your password is:', get_password_strength(args.password))
